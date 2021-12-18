383bf901aa782b8634dc6875f666ba60b704b66c
Javier Medina <javier_j_medina@hotmail.com>
2021-12-18 16:04:46 -0300

    primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Table from './components/Tablecoins';
+
 
 function App() {
+
+  const getData = async()=> {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    console.log(res.data)
+  }
+
+
+  useEffect(() => {
+    getData()
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Coin market</h1>
+        <Tablecoins/>
     </div>
   );
 }
---
+++ src/components/Tablecoins.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Tablecoins = () => {
+    return (
+        <div>
+            Table
+        </div>
+    )
+}
+
+export default Tablecoins;
