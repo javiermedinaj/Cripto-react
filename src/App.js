60c88c871d7297d2a8a3d9a439871c40018f3d0d
Javier Medina <javier_j_medina@hotmail.com>
2021-12-22 16:33:31 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Table from './components/Tablecoins';
-
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import TableCoins from "./components/Tablecoins";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const getData = async()=> {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    console.log(res.data)
-  }
-
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      setCoins(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    getData()
-  },[]);
+    getData();
+  }, []);
 
   return (
-    <div className="App">
-        <h1>Coin market</h1>
-        <Tablecoins/>
+    <div className="container">
+      <div className="row">
+        <input
+          type="text"
+          placeholder="Search a Coin"
+          className="form-control bg-dark text-light border-0 mt-4 text-center"
+          autoFocus
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <TableCoins coins={coins} search={search} />
+      </div>
     </div>
   );
 }
